3fa86c3bf2772e0f130986d5dbc7ad96884b5792
Brandon Berney <brandonberney@protonmail.com>
2015-09-14 13:53:56 -0400

    Add code

--- src/rightTriangle.java
+++ src/rightTriangle.java
@@ -1,6 +1,7 @@
 /**
  * Author: Brandon B.
  * Date: 9-10-15
+ * Description: Determines whether or not a triangle is a right triangle.
  */
  
 import java.util.Scanner;
@@ -32,63 +33,34 @@ public class rightTriangle {
     	largestSide = Math.max(side1, Math.max(side2, side3));
     	cSquared = Math.pow(largestSide, 2);
 
-		// Run the numbers through some checks.
-		if (side1 != largestSide) {
-			// Side 1 is NOT the largest side.
-			secondarySide = side1;
+		if ((side1 == largestSide && (side2 == largestSide || side3 == largestSide))
+			|| (side2 == largestSide && (side1  == largestSide || side3 == largestSide)) ||
+				(side3 == largestSide && (side1 == largestSide || side2 == largestSide))) {
+			// More than one side is the largest! It's safe to assume this isn't a right triangle.
+			isRightTriangle = false;
 		}
-
-		if (side2 != largestSide) {
-			// Side 2 is NOT the largest side.
-			if (secondarySide == null) {
-				// Secondary side hasn't been set yet. (1 was the largest?)
-				secondarySide = side2;
-			}
-			else {
-				// We'll use the backup.
-				secondarySide2 = side2;
-		}
-
-		if (side3 != largestSide) {
-			// Side 3 is NOT the largest side.
-			if (secondarySide == null) {
-				// Secondary side hasn't been set yet. (1 or 2 was the largest?)
-				secondarySide = side2;
-			}
-			else {
-				// We'll use the backup.
-				secondarySide2 = side2;
-		}
-
-		if ()
 		
-		/* if (side1 == side2) {
-			// Side 1 is equal to Side 2!
-			if (largestSide == side1) {
-				// Side 1 and 2 are both the largest sides.
-				mathResults = Math.pow(side1, 2) + Math.pow(side2, 2);
-			}
+		// Run the numbers through some checks.
+		if (largestSide == side1) {
+			secondarySide = side2;
+			secondarySide2 = side3;
+		}
+		else if(largestSide == side2){
+			secondarySide = side1;
+			secondarySide2 = side3;
+		}
+		else{
+			secondarySide = side1;
+			secondarySide2 = side2;
 		}
 
-		else if (side1 == side3) {
-			//Side 1 is equal to Side 3!
-			if (largestSide == side1) {
-				//Side 1 and 3 are both the largest sides.
-				mathResults = Math.pow(side1, 2) + Math.pow(side3, 2);
-			}
-		}
+		// Square the two small sides and add them together.
+		mathResults = Math.pow(secondarySide, 2) + Math.pow(secondarySide2, 2);
+		if (mathResults == Math.pow(largestSide, 2)) // Is the largest side (squared) equal to our equation above?
+			isRightTriangle = true;
+		else
+			isRightTriangle = false;
 
-		else if (side2 == side3) {
-			//Side 2 is equal to Side 3!
-			if (largestSide == side2) {
-				//Side 2 and 3 are both the largest sides.
-				mathResults = Math.pow(side1, 2) + Math.pow(side3, 2);
-			}
-		}
-
-		else {
-			mathResults = Math.pow
-		} */
-    	System.out.println(":debug: Largest side is " + largestSide + "m!");
+		System.out.println("Your triangle is a right triangle: " + isRightTriangle);
     }
 }
